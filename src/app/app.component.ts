ef54f4387807c4a4b7a766e110aab9d5b93f9486
Sefa Ecir <sefacir@hotmail.com>
2025-04-30 17:06:52 +0300

    Initial commit - Angular user list app (BIM308 HW3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { UserService, User } from './user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'user-list-app';
+export class AppComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
 }
